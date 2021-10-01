1cc173507454093a75b0d1cff150ffb40ab0137a
KoziTheWolf <91115600+KoziTheWolf@users.noreply.github.com>
2021-10-01 15:14:59 +0200

    Add files via upload

---
+++ pesel.py
@@ -0,0 +1,21 @@
+
+
+pesel = str(input('Podaj pesel'))
+
+def plec(pesel):
+    if int(pesel[10])%2 !=0:
+        print('K')
+    else:
+        print('M')
+
+
+def sprawdz(pesel):
+
+    if len(pesel)==11:
+        plec(pesel)
+    else:
+        print('nieprawidlowy pesel')
+        print('wprowadz ponownie')
+        pesel = str(input('Podaj pesel'))
+        sprawdz(pesel)
+sprawdz(pesel)
